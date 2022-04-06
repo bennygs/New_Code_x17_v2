2aebdb6e63423c5cf7495e761854e0453100e605
benito <benito@nuclex-200.lnl.infn.it>
2022-04-06 18:09:06 +0200

    Detector construction p.1

--- B4c/src/B4cDetectorConstruction.cc
+++ B4c/src/B4cDetectorConstruction.cc
@@ -23,7 +23,7 @@
 // * acceptance of all terms of the Geant4 Software license.          *
 // ********************************************************************
 //
-// 
+//
 /// \file B4cDetectorConstruction.cc
 /// \brief Implementation of the B4cDetectorConstruction class
 
@@ -47,10 +47,17 @@
 #include "G4PhysicalConstants.hh"
 #include "G4SystemOfUnits.hh"
 
+#include "G4RunManager.hh"
+#include "G4Cons.hh"
+#include "G4Orb.hh"
+#include "G4Sphere.hh"
+#include "G4Trd.hh"
+#include "G4AssemblyVolume.hh"
+#include "G4RotationMatrix.hh"
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-G4ThreadLocal 
-G4GlobalMagFieldMessenger* B4cDetectorConstruction::fMagFieldMessenger = 0; 
+G4ThreadLocal
+G4GlobalMagFieldMessenger* B4cDetectorConstruction::fMagFieldMessenger = 0;
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
@@ -64,16 +71,16 @@ B4cDetectorConstruction::B4cDetectorConstruction()
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 B4cDetectorConstruction::~B4cDetectorConstruction()
-{ 
-}  
+{
+}
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 G4VPhysicalVolume* B4cDetectorConstruction::Construct()
 {
-  // Define materials 
+  // Define materials
   DefineMaterials();
-  
+
   // Define volumes
   return DefineVolumes();
 }
@@ -81,15 +88,16 @@ G4VPhysicalVolume* B4cDetectorConstruction::Construct()
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 void B4cDetectorConstruction::DefineMaterials()
-{ 
+{
   // Lead material defined using NIST Manager
   auto nistManager = G4NistManager::Instance();
   nistManager->FindOrBuildMaterial("G4_Pb");
-  
+  nistManager->FindOrBuildMaterial("G4_PLASTIC_SC_VINYLTOLUENE");
+
   // Liquid argon material
   G4double a;  // mass of a mole;
-  G4double z;  // z=mean number of protons;  
-  G4double density; 
+  G4double z;  // z=mean number of protons;
+  G4double density;
   new G4Material("liquidArgon", z=18., a= 39.95*g/mole, density= 1.390*g/cm3);
          // The argon by NIST Manager is a gas with a different density
 
@@ -114,74 +122,225 @@ G4VPhysicalVolume* B4cDetectorConstruction::DefineVolumes()
   auto layerThickness = absoThickness + gapThickness;
   auto calorThickness = fNofLayers * layerThickness;
   auto worldSizeXY = 1.2 * calorSizeXY;
-  auto worldSizeZ  = 1.2 * calorThickness; 
-  
+  auto worldSizeZ  = 1.2 * calorThickness;
+
   // Get materials
   auto defaultMaterial = G4Material::GetMaterial("Galactic");
   auto absorberMaterial = G4Material::GetMaterial("G4_Pb");
   auto gapMaterial = G4Material::GetMaterial("liquidArgon");
-  
+  auto Scintillator_Material = G4Material::GetMaterial("G4_PLASTIC_SC_VINYLTOLUENE");
+
   if ( ! defaultMaterial || ! absorberMaterial || ! gapMaterial ) {
     G4ExceptionDescription msg;
-    msg << "Cannot retrieve materials already defined."; 
+    msg << "Cannot retrieve materials already defined.";
     G4Exception("B4DetectorConstruction::DefineVolumes()",
       "MyCode0001", FatalException, msg);
-  }  
-   
-  //     
+  }
+
+  //
   // World
   //
-  auto worldS 
+  auto worldS
     = new G4Box("World",           // its name
                  worldSizeXY/2, worldSizeXY/2, worldSizeZ/2); // its size
-                         
+
   auto worldLV
     = new G4LogicalVolume(
                  worldS,           // its solid
                  defaultMaterial,  // its material
                  "World");         // its name
-                                   
+
   auto worldPV
     = new G4PVPlacement(
                  0,                // no rotation
                  G4ThreeVector(),  // at (0,0,0)
-                 worldLV,          // its logical volume                         
+                 worldLV,          // its logical volume
                  "World",          // its name
                  0,                // its mother  volume
                  false,            // no boolean operation
                  0,                // copy number
-                 fCheckOverlaps);  // checking overlaps 
-  
-  //                               
-  // Calorimeter
-  //  
-  auto calorimeterS
-    = new G4Box("Calorimeter",     // its name
-                 calorSizeXY/2, calorSizeXY/2, calorThickness/2); // its size
-                         
-  auto calorLV
+                 fCheckOverlaps);  // checking overlaps
+
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+//&&&&&&&&&&&&&&&&&&&&&&&&&  Detector Sizes  &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+G4int N_HorBars = 10;
+G4int N_VerBars = 10;
+
+//  Calorimeter
+G4double Cal_x = 10.0*cm;
+G4double Cal_y = 5.0*cm;
+G4double Cal_z = 5.0*cm;
+
+//***************************** Bars *******************************************
+
+G4double bar_Thickness = 0.2*cm;
+
+//  Horizontal Bars
+
+G4double HorBar_x = bar_Thickness;
+G4double HorBar_y = Cal_y/N_HorBars;
+G4double HorBar_z = Cal_z;
+
+//  Vertical Bars
+
+G4double VerBar_x = bar_Thickness;
+G4double VerBar_y = Cal_y;
+G4double VerBar_z = Cal_z/N_VerBars;
+
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+//&&&&&&&&&&&&&&&&&&  Detector Elements Construction  &&&&&&&&&&&&&&&&&&&&&&&&&&
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+//==============================================================================
+
+//***************************** Bars *******************************************
+
+//------------------------------------------------------------------------------
+// Horizontal bars
+
+  auto HorBarS
+    = new G4Box("HorBar",           //its name
+    HorBar_x/2, HorBar_y/2, HorBar_z/2); //its size
+
+auto HorBarLV
     = new G4LogicalVolume(
-                 calorimeterS,     // its solid
+      HorBarS,                    // its solid
+      Scintillator_Material,       // its material
+      "HorBar");                  // its name
+
+new G4PVPlacement(
+                  0,                // no rotation
+                  G4ThreeVector(),  // at (0,0,0)
+                  HorBarLV,          // its logical volume
+                  "HorBar",    // its name
+                  worldLV,          // its mother  volume
+                  false,            // no boolean operation
+                  0,                // copy number
+                  fCheckOverlaps);  // checking overlaps
+
+//------------------------------------------------------------------------------
+
+// Vertical bars
+
+  auto VerBarS
+        = new G4Box("HorBar",           //its name
+          VerBar_x/2, VerBar_y/2, VerBar_z/2); //its size
+
+  auto VerBarLV
+        = new G4LogicalVolume(
+          VerBarS,                    // its solid
+          Scintillator_Material,       // its material
+          "VerBar");                  // its name
+
+  new G4PVPlacement(
+                    0,                // no rotation
+                    G4ThreeVector(),  // at (0,0,0)
+                    VerBarLV,          // its logical volume
+                    "VerBar",    // its name
+                    worldLV,          // its mother  volume
+                    false,            // no boolean operation
+                    0,                // copy number
+                    fCheckOverlaps);  // checking overlaps
+
+//==============================================================================
+
+//*************************** Calorimeter **************************************
+
+  auto CalorimeterS
+        = new G4Box("Calorimeter",
+          Cal_x/2, Cal_y/2, Cal_z/2);
+
+  auto CalorimeterLV
+        = new G4LogicalVolume(
+          CalorimeterS,                    // its solid
+          Scintillator_Material,       // its material
+          "Calorimeter");                  // its name
+
+  new G4PVPlacement(
+                    0,                // no rotation
+                    G4ThreeVector(),  // at (0,0,0)
+                    CalorimeterLV,          // its logical volume
+                    "Calorimeter",    // its name
+                    worldLV,          // its mother  volume
+                    false,            // no boolean operation
+                    0,                // copy number
+                    fCheckOverlaps);  // checking overlaps
+
+//==============================================================================
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&  Clover assembly  &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+
+//==============================================================================
+//Assemblying the Clover
+//==============================================================================
+
+
+G4AssemblyVolume* assemblyCloverDetectors = new G4AssemblyVolume();
+
+G4double detSou_Dis = 10.*cm;		//Source-Detector Distance
+
+// Translation and Rotation of a clover inside the assembly
+
+   G4ThreeVector T_Cal_0;
+   G4ThreeVector T_Cal;
+   G4RotationMatrix* R_Cal = new G4RotationMatrix;
+
+   G4ThreeVector T_HorBar_0;
+   G4ThreeVector T_HorBar;
+   G4RotationMatrix* R_HorBar = new G4RotationMatrix;
+
+
+   G4ThreeVector T_VerBar_0;
+   G4ThreeVector T_VerBar;
+   G4RotationMatrix* R_VerBar = new G4RotationMatrix;
+
+ // Rotation of the assembly inside the world
+    G4ThreeVector Tm;
+    G4RotationMatrix* Rm = new G4RotationMatrix;
+
+
+
+
+
+
+//555555555555555555555555555555555555555555555555555555555555555555555555555555
+  //
+  // Calorimeter
+  //
+  auto containerS
+    = new G4Box("container",     // its name
+                 calorSizeXY/2, calorSizeXY/2, calorThickness/2); // its size
+
+  auto containerLV
+    = new G4LogicalVolume(
+                 containerS,     // its solid
                  defaultMaterial,  // its material
-                 "Calorimeter");   // its name
-                                   
+                 "container");   // its name
+
   new G4PVPlacement(
                  0,                // no rotation
                  G4ThreeVector(),  // at (0,0,0)
-                 calorLV,          // its logical volume                         
-                 "Calorimeter",    // its name
+                 containerLV,          // its logical volume
+                 "container",    // its name
                  worldLV,          // its mother  volume
                  false,            // no boolean operation
                  0,                // copy number
-                 fCheckOverlaps);  // checking overlaps 
-   
-  //                                 
+                 fCheckOverlaps);  // checking overlaps
+
+  //
   // Layer
   //
-  auto layerS 
+  auto layerS
     = new G4Box("Layer",           // its name
                  calorSizeXY/2, calorSizeXY/2, layerThickness/2); //its size
-                         
+
   auto layerLV
     = new G4LogicalVolume(
                  layerS,           // its solid
@@ -191,77 +350,77 @@ G4VPhysicalVolume* B4cDetectorConstruction::DefineVolumes()
   new G4PVReplica(
                  "Layer",          // its name
                  layerLV,          // its logical volume
-                 calorLV,          // its mother
+                 containerLV,          // its mother
                  kZAxis,           // axis of replication
                  fNofLayers,        // number of replica
                  layerThickness);  // witdth of replica
-  
-  //                               
+
+  //
   // Absorber
   //
-  auto absorberS 
+  auto absorberS
     = new G4Box("Abso",            // its name
                  calorSizeXY/2, calorSizeXY/2, absoThickness/2); // its size
-                         
+
   auto absorberLV
     = new G4LogicalVolume(
                  absorberS,        // its solid
                  absorberMaterial, // its material
                  "AbsoLV");        // its name
-                                   
+
    new G4PVPlacement(
                  0,                // no rotation
                  G4ThreeVector(0., 0., -gapThickness/2), // its position
-                 absorberLV,       // its logical volume                         
+                 absorberLV,       // its logical volume
                  "Abso",           // its name
                  layerLV,          // its mother  volume
                  false,            // no boolean operation
                  0,                // copy number
-                 fCheckOverlaps);  // checking overlaps 
+                 fCheckOverlaps);  // checking overlaps
 
-  //                               
+  //
   // Gap
   //
-  auto gapS 
+  auto gapS
     = new G4Box("Gap",             // its name
                  calorSizeXY/2, calorSizeXY/2, gapThickness/2); // its size
-                         
+
   auto gapLV
     = new G4LogicalVolume(
                  gapS,             // its solid
                  gapMaterial,      // its material
                  "GapLV");         // its name
-                                   
+
   new G4PVPlacement(
                  0,                // no rotation
                  G4ThreeVector(0., 0., absoThickness/2), // its position
-                 gapLV,            // its logical volume                         
+                 gapLV,            // its logical volume
                  "Gap",            // its name
                  layerLV,          // its mother  volume
                  false,            // no boolean operation
                  0,                // copy number
-                 fCheckOverlaps);  // checking overlaps 
- 
+                 fCheckOverlaps);  // checking overlaps
+
   //
   // print parameters
   //
   G4cout
-    << G4endl 
+    << G4endl
     << "------------------------------------------------------------" << G4endl
     << "---> The calorimeter is " << fNofLayers << " layers of: [ "
-    << absoThickness/mm << "mm of " << absorberMaterial->GetName() 
+    << absoThickness/mm << "mm of " << absorberMaterial->GetName()
     << " + "
     << gapThickness/mm << "mm of " << gapMaterial->GetName() << " ] " << G4endl
     << "------------------------------------------------------------" << G4endl;
-  
-  //                                        
+
+  //
   // Visualization attributes
   //
   worldLV->SetVisAttributes (G4VisAttributes::GetInvisible());
 
   auto simpleBoxVisAtt= new G4VisAttributes(G4Colour(1.0,1.0,1.0));
   simpleBoxVisAtt->SetVisibility(true);
-  calorLV->SetVisAttributes(simpleBoxVisAtt);
+  containerLV->SetVisAttributes(simpleBoxVisAtt);
 
   //
   // Always return the physical World
@@ -275,31 +434,31 @@ void B4cDetectorConstruction::ConstructSDandField()
 {
   // G4SDManager::GetSDMpointer()->SetVerboseLevel(1);
 
-  // 
+  //
   // Sensitive detectors
   //
-  auto absoSD 
+  auto absoSD
     = new B4cCalorimeterSD("AbsorberSD", "AbsorberHitsCollection", fNofLayers);
   G4SDManager::GetSDMpointer()->AddNewDetector(absoSD);
   SetSensitiveDetector("AbsoLV",absoSD);
 
-  auto gapSD 
+  auto gapSD
     = new B4cCalorimeterSD("GapSD", "GapHitsCollection", fNofLayers);
   G4SDManager::GetSDMpointer()->AddNewDetector(gapSD);
   SetSensitiveDetector("GapLV",gapSD);
 
-  // 
+  //
   // Magnetic field
   //
   // Create global magnetic field messenger.
   // Uniform magnetic field is then created automatically if
   // the field value is not zero.
-  G4ThreeVector fieldValue;
+/*G4ThreeVector fieldValue;
   fMagFieldMessenger = new G4GlobalMagFieldMessenger(fieldValue);
   fMagFieldMessenger->SetVerboseLevel(1);
-  
+
   // Register the field messenger for deleting
-  G4AutoDelete::Register(fMagFieldMessenger);
+  G4AutoDelete::Register(fMagFieldMessenger);*/
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
